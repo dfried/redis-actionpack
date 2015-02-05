85d7abafc51c89c93639ece1629f88cd2a54b7e5
Dan Fried <daniel.k.fried@gmail.com>
2015-02-05 15:02:37 -0500

    Try to fix redis cookie domains, like in https://github.com/redis-store/redis-actionpack/pull/2/files

--- lib/action_dispatch/middleware/session/redis_store.rb
+++ lib/action_dispatch/middleware/session/redis_store.rb
@@ -7,11 +7,20 @@ module ActionDispatch
     class RedisStore < Rack::Session::Redis
       include Compatibility
       include StaleSessionCheck
+      include SessionObject
+
       def initialize(app, options = {})
         options = options.dup
         options[:redis_server] ||= options[:servers]
         super
       end
+
+      private
+      #This is from https://github.com/redis-store/redis-actionpack/pull/2/files
+
+      def set_cookie(env, session_id, cookie)
+        request = ActionDispatch::Request.new(env)
+        request.cookie_jar[key] = cookie
     end
   end
 end
